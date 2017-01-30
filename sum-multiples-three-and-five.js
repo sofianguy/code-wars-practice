19a3c3b2a2949c09e6d0578b1328b2186b54d5de
Sofia Nguy <sofianguy@gmail.com>
2017-01-30 15:24:35 -0800

    sum of multiples of 3 or 5

--- sum-multiples-three-and-five.js
+++ sum-multiples-three-and-five.js
@@ -4,3 +4,15 @@
 // of 3 or 5 below the number passed in.
 // Note: If the number is a multiple of both 3 and 5, only count it once.
 
+function solution(number){
+  var sumMultiples = 0;
+  for (var i=1; i<number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sumMultiples = sumMultiples + i;
+      console.log('multiple of 3 or 5: ', i)
+    }
+  }
+  return sumMultiples;
+}
+
+console.log(solution(10));

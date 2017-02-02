ff5482b8b49f7fd0dda652f87d6891c247679d58
Sofia Nguy <sofianguy@gmail.com>
2017-02-02 14:25:53 -0800

    opposite number in one line

---
+++ opposite-number.js
@@ -0,0 +1,13 @@
+// Very simple, given a number, find its opposite.
+// Examples:
+// 1: -1
+// 14: -14
+// -34: 34
+// But can you do it in 1 line of code and without any conditionals?
+
+function opposite(number) {
+	return number*-1;
+}
+
+console.log(opposite(-1)); // 1
+console.log(opposite(1)); // -1

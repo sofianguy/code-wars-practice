d237e27a92d2163c021a919d7b8ab9f35af95ca8
Sofia Nguy <sofianguy@gmail.com>
2017-02-10 12:31:15 -0800

    sum of the first Nth term of a series - problem description

---
+++ sum-first-nth-term-series.js
@@ -0,0 +1,19 @@
+// Your task is to write a function which returns the sum of following series
+// upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer upto 2 decimal places and return it as String.
+// If the given value is 0 then it should return 0.00
+// You will only be given Natural Numbers as arguments.
+
+// Examples:
+// SeriesSum(1) => 1 = "1"
+// SeriesSum(2) => 1 + 1/4 = "1.25"
+// SeriesSum(5) => 1 + 1/4 + 1/7 + 1/10 + 1/13 = "1.57"
+
+function seriesSum(n) {
+
+}
+
+console.log(seriesSum(1)); // 

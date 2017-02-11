69db008e8859c529d437401c118da71454324b2c
Sofia Nguy <sofianguy@gmail.com>
2017-02-10 16:06:42 -0800

    first solution

--- sum-first-nth-term-series.js
+++ sum-first-nth-term-series.js
@@ -13,7 +13,30 @@
 // SeriesSum(5) => 1 + 1/4 + 1/7 + 1/10 + 1/13 = "1.57"
 
 function seriesSum(n) {
+  var series = [1, 1/4];
 
+  if (n === 0) {
+    return '0.00';
+  } else if (n === 1) {
+    return series[0].toFixed(2);
+  } else if (n === 2) {
+    var sum = series[0] + series[1];
+    return sum.toString();
+  }
+
+  while (series.length < n) {
+    console.log('while loop n: ', series.length);
+    // formula for sequence pattern: 1/3n+1
+    var temp = 1 / (3*series.length + 1);
+    console.log('temp: ', temp)
+    series.push(temp);
+  }
+
+  var sumSeries = series.reduce(function (a,b) {
+    return a + b;
+  });
+
+  return sumSeries.toFixed(2);
 }
 
 console.log(seriesSum(1)); // 

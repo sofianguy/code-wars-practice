e7f7028b5a3c602cc521427c19608185124bb86a
Sofia Nguy <sofianguy@gmail.com>
2017-02-10 16:15:12 -0800

    indentation?

--- tribonacci.js
+++ tribonacci.js
@@ -1,29 +1,29 @@
-// Tribonacci works basically like a Fibonacci, but summing the 
+// Tribonacci works basically like a Fibonacci, but summing the
 // last 3 (instead of 2) numbers of the sequence to generate the next.
 
-// So, if we are to start our Tribonacci sequence with [1,1,1] as 
+// So, if we are to start our Tribonacci sequence with [1,1,1] as
 // a starting input (AKA signature), we have this sequence:
 // [1,1,1,3,5,9,17,31,...]
 
-// But what if we started with [0,0,1] as a signature? 
-// As starting with [0,1] instead of [1,1] basically shifts the 
-// common Fibonacci sequence by once place, you may be tempted to 
-// think that we would get the same sequence shifted by 2 places, 
+// But what if we started with [0,0,1] as a signature?
+// As starting with [0,1] instead of [1,1] basically shifts the
+// common Fibonacci sequence by once place, you may be tempted to
+// think that we would get the same sequence shifted by 2 places,
 // but that is not the case and we would get:
 // [0,0,1,1,2,4,7,13,24,...]
 
-// Well, you may have guessed it by now, but to be clear: 
-// you need to create a fibonacci function that given a 
-// signature array/list, returns the first n elements - signature 
+// Well, you may have guessed it by now, but to be clear:
+// you need to create a fibonacci function that given a
+// signature array/list, returns the first n elements - signature
 // included of the so seeded sequence.
 
-// Signature will always contain 3 numbers; 
-// n will always be a non-negative number; 
-// if n==0, then return an empty array and be 
+// Signature will always contain 3 numbers;
+// n will always be a non-negative number;
+// if n==0, then return an empty array and be
 // ready for anything else which is not clearly specified.
 
 
-// tribonacci takes two parameters: 
+// tribonacci takes two parameters:
 // 1. the starter sequence -- sequence
 // 2. number of length of sequence -- n
 function tribonacci(signature, n) {
@@ -36,7 +36,7 @@ function tribonacci(signature, n) {
 
 	while (triSeq.length < n) {
 		// add to triSeq array the sum of the three preceding numbers in triSeq array.
-		triSeq.push(triSeq[triSeq.length - 3] + 
+		triSeq.push(triSeq[triSeq.length - 3] +
 			triSeq[triSeq.length - 2] + triSeq[triSeq.length -1]);
 	}
 

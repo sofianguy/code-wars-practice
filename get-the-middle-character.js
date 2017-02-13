38de9d7b291b1d6724185eefdc135cc9222cf76b
Sofia Nguy <sofianguy@gmail.com>
2017-02-12 22:31:22 -0800

    find middle character of a string question description

---
+++ get-the-middle-character.js
@@ -0,0 +1,14 @@
+// You are going to be given a word. Your job is to return the middle
+// character of the word. If the word's length is odd, return the middle
+// character. If the word's length is even, return the middle 2 characters.
+// Examples:
+// Kata.getMiddle("test") should return "es"
+// Kata.getMiddle("testing") should return "t"
+// Kata.getMiddle("middle") should return "dd"
+// Kata.getMiddle("A") should return "A"
+// Input: A word (string) of length 0 < str < 1000
+// Output: The middle character(s) of the word represented as a string.
+
+function getMiddle(string) {
+  
+}

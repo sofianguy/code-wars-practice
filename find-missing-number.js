e58f762dd68f46e9cb06ecc2b0321861d9c6d70e
Sofia Nguy <sofianguy@gmail.com>
2017-02-13 15:28:26 -0800

    alternate solutions; subtracting sum of unsorted array from total sum of array

--- find-missing-number.js
+++ find-missing-number.js
@@ -8,8 +8,8 @@
 // inclusively. However, one number is missing. Write a function to find and
 // return this number. What are the time and space complexities of your solution?
 
-function missingNum(array) {
-  var sortedArray = array.sort();
+function missingNum(unsortedArray) {
+  var sortedArray = unsortedArray.sort();
 
   for (var i=0; i<sortedArray.length; i++) {
     if (i !== sortedArray[i]) {
@@ -19,3 +19,33 @@ function missingNum(array) {
 }
 
 console.log(missingNum([0,1,2,3,5])); // 4
+
+// --------------------------------------------------------------------------
+function missingNum1(unsortedArray) {
+  // unknown sum of numbers in array including missing number
+  var shouldBeSum = 0;
+  for (var i=0; i<=unsortedArray.length; i++) {
+    shouldBeSum = shouldBeSum + i;
+  }
+
+  var unsortedSum = 0;
+  for (var i=0; i<unsortedArray.length; i++) {
+    unsortedSum = unsortedSum + unsortedArray[i];
+  }
+
+  return shouldBeSum - unsortedSum;
+}
+
+console.log(missingNum1([0,1,2,3,5])); // 4
+
+// --------------------------------------------------------------------------
+function missingNum2(unsortedArray) {
+  var unsortedSum = 0;
+  
+  for (var i=0; i<unsortedArray.length; i++) {
+    unsortedSum = unsortedSum + unsortedArray[i];
+  }
+
+  // 5050 is total sum of 0 to 100
+  return 5050 - unsortedSum;
+}

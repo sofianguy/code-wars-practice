12185540481d58cecb1e71316da376672915b41b
Sofia Nguy <sofianguy@gmail.com>
2017-02-13 12:26:02 -0800

    alternate solution using switch-case

--- basic-mathematical-operations.js
+++ basic-mathematical-operations.js
@@ -23,3 +23,19 @@ function basicOp(operation, value1, value2) {
 }
 
 console.log(basicOp('+', 4, 7)); // 11
+
+// ----------------------------------------------------------------------------
+function basicOp1(operation, value1, value2) {
+  switch (operation) {
+    case '+':
+      return value1 + value2;
+    case '-':
+      return value1 - value2;
+    case '*':
+      return value1 * value2;
+    case '/':
+      return value1 / value2;
+  }
+}
+
+console.log(basicOp1('+', 4, 7)); // 

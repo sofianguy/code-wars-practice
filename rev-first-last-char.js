b0568c9b22f73c6f500ef670d443d7c34380960e
Sofia Nguy <sofianguy@gmail.com>
2017-02-14 20:26:46 -0800

    run function with different string

--- rev-first-last-char.js
+++ rev-first-last-char.js
@@ -20,4 +20,5 @@ function middleCharacter(str) {
   }
 }
 
-console.log(middleCharacter('1,2,3')); //
+console.log(middleCharacter('1,2,3')); // '2'
+console.log(middleCharacter('1,2,3,4,5,6,7,8,9')); // '2 3 4 5 6 7 8'

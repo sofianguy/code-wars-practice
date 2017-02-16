cafd0fb5ad12f0860b16fa36116e0b0be41fd978
Sofia Nguy <sofianguy@gmail.com>
2017-02-15 16:43:00 -0800

    second solution

--- who-likes-it.js
+++ who-likes-it.js
@@ -28,3 +28,21 @@ function likes(names) {
 }
 console.log(likes(["Alex", "Jacob", "Mark", "Max"]));
 // 'Alex, Jacob and 2 others like this'
+
+// ---------------------------------------------------------------------------
+function likes1(names) {
+  switch(names.length) {
+    case 0:
+      return 'no one likes this';
+    case 1:
+      return names[0] + ' likes this';
+    case 2:
+      return names[0] + ' and ' + names[1] + ' like this';
+    case 3:
+      return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this';
+    case 4:
+      return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+  }
+}
+console.log(likes(["Alex", "Jacob", "Mark", "Max"]));
+// 'Alex, Jacob and 2 others like this'

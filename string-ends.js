abcdb4954744d3e31e964cc3d84e6dc69e94b64e
Sofia Nguy <sofianguy@gmail.com>
2017-02-16 15:59:30 -0800

    return values

--- string-ends.js
+++ string-ends.js
@@ -13,9 +13,9 @@ function stringEnds(str, ends) {
     return false;
   }
 }
-console.log(stringEnds('abc', 'bc'), true);
-console.log(stringEnds('abc', 'b'), false);
-console.log(stringEnds('', ''), true);
-console.log(stringEnds('', 'asdf'), false);
-console.log(stringEnds('aaa', 'aa'), true);
-console.log(stringEnds('asdf', ''), true);
+console.log(stringEnds('abc', 'bc'), true); // true
+console.log(stringEnds('abc', 'b'), false); // false
+console.log(stringEnds('', ''), true); // true
+console.log(stringEnds('', 'asdf'), false); // false
+console.log(stringEnds('aaa', 'aa'), true); // true
+console.log(stringEnds('asdf', ''), true); // true

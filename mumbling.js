e4c7cb10815c3fd533430da38989b2dce766efc0
Sofia Nguy <sofianguy@gmail.com>
2017-02-27 15:39:21 -0800

    solution using Array().join()

--- mumbling.js
+++ mumbling.js
@@ -19,8 +19,9 @@ console.log(accum("RqaEzty")); // 'R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy'
 function accum1(s) {
   var output = [];
   for (var i=0; i<s.length; i++) {
-    output.push(s[i].toUpperCase() + Array(i-1).toLowerCase().join(s[i]));
+    output.push(s[i].toUpperCase() + Array(i+1).join(s[i]).toLowerCase());
   }
+  return output.join('-');
 }
-console.log(accum("abcd")); // 'A-Bb-Ccc-Dddd'
-console.log(accum("RqaEzty")); // 'R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy'
+console.log(accum1("abcd")); // 'A-Bb-Ccc-Dddd'
+console.log(accum1("RqaEzty")); // 'R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy'

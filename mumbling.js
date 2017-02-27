1ddf4822ff6b977d9802853c0317618a6a972962
Sofia Nguy <sofianguy@gmail.com>
2017-02-27 15:31:47 -0800

    solution - return new string with repeat letters

---
+++ mumbling.js
@@ -0,0 +1,16 @@
+// Write function accum:
+// Examples:
+// accum("abcd");    // "A-Bb-Ccc-Dddd"
+// accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt");    // "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+  var output = [];
+  for (var i=0; i<s.length; i++) {
+    output.push(s[i].toUpperCase() + s[i].toLowerCase().repeat(i));
+  }
+  return output.join('-');
+}
+console.log(accum("abcd")); // 'A-Bb-Ccc-Dddd'
+console.log(accum("RqaEzty")); // 'R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy'

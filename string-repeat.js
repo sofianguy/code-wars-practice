1dbc073f3bce64f4866cb6049fe4ca139a5de549
Sofia Nguy <sofianguy@gmail.com>
2017-02-27 16:05:46 -0800

    solution using while loop

--- string-repeat.js
+++ string-repeat.js
@@ -19,10 +19,35 @@ console.log(repeatStr1(5, "Hello")); // "HelloHelloHelloHelloHello"
 // ------------------------------------------------------------------
 function repeatStr2(n,s) {
   var output = ''
-  for (var i=0; i<=n; i++) {
+  for (var i=0; i<n; i++) {
     output = output + s;
   }
   return output;
 }
 console.log(repeatStr2(6, "I")); // "IIIIII"
 console.log(repeatStr2(5, "Hello")); // "HelloHelloHelloHelloHello"
+
+// ------------------------------------------------------------------
+function repeatStr3(n,s) {
+  var output = '';
+  var i=0;
+  while (i < n) {
+    output = output + s;
+    i++;
+  }
+  return output;
+}
+console.log(repeatStr3(6, "I")); // "IIIIII"
+console.log(repeatStr3(5, "Hello")); // "HelloHelloHelloHelloHello"
+
+// ------------------------------------------------------------------
+function repeatStr4(n,s) {
+  var output = '';
+  while (n > 0) {
+    output = output + s;
+    n--;
+  }
+  return output;
+}
+console.log(repeatStr4(6, "I")); // "IIIIII"
+console.log(repeatStr4(5, "Hello")); // "HelloHelloHelloHelloHello"

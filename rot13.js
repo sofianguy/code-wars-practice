193e884c72b6b838d7421f2472a13ef2fc704914
Sofia Nguy <sofianguy@gmail.com>
2017-03-30 20:18:15 -0700

    solution for rot13 encryption; tests for mixed uppercase, lowercase, punctuation, spaces, numbers

--- rot13.js
+++ rot13.js
@@ -38,10 +38,45 @@ function rot13(message) {
     'y':'l',
     'z':'m'
   };
-  return alphabet[message];
+
+  var messageArr = message.split('');
+  var output = [];
+  for (var i=0; i<messageArr.length; i++) {
+    var isUppercase = messageArr[i].toUpperCase() === messageArr[i];
+    var isLowercase = messageArr[i].toLowerCase() === messageArr[i];
+    if (alphabet.hasOwnProperty(messageArr[i]) && isLowercase) {
+      output.push(alphabet[messageArr[i]]);
+    } else if (alphabet.hasOwnProperty(messageArr[i].toLowerCase()) && isUppercase) {
+      output.push(alphabet[messageArr[i].toLowerCase()].toUpperCase());
+    } else {
+      output.push(messageArr[i]);
+    }
+  }
+  return output.join('');
 }
+
 var {test} = require('tap');
 test('1 - test one lowercase letter t should return g', t => {
   t.equals(rot13('t'), 'g');
+  t.end();
+});
+
+test('2 - one lowercase word', t => {
+  t.equals(rot13('test'), 'grfg');
+  t.end();
+});
+
+test('3 - one uppercase letter', t => {
+  t.equals(rot13('T'), 'G');
+  t.end();
+});
+
+test('4 - uppercase and lowercase input', t => {
+  t.equals(rot13('Codewars'), 'Pbqrjnef');
+  t.end();
+});
+
+test('5 - uppercase, lowercase, numbers, punctuation, spaces', t => {
+  t.equals(rot13('10+2 is twelve.'), '10+2 vf gjryir.');
   t.end();
 });

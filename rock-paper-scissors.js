afa2fa214e9fa5b3206624aa7d90152984f07806
Sofia Nguy <sofianguy@gmail.com>
2017-04-11 15:06:22 -0700

    second solution using if/else

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -22,3 +22,18 @@ function rps(p1, p2) {
 console.log(rps('scissors', 'rock'), 'Player 2 won!');
 console.log(rps('scissors','paper'), 'Player 1 won!');
 console.log(rps('paper','paper'), 'Draw!');
+
+
+
+function rps1(p1, p2) {
+  if (p1 == p2){
+    return "Draw!";
+  } else if ((p1 == "scissors" && p2 == "paper") || (p1 == "paper" && p2 == "rock") || (p1 == "rock" && p2 == "scissors")){
+    return "Player 1 won!";
+  } else if ((p2 == "scissors" && p1 == "paper") || (p2 == "paper" && p1 == "rock") || (p2 == "rock" && p1 == "scissors")){
+    return "Player 2 won!";
+  }
+}
+console.log(rps1('scissors', 'rock'), 'Player 2 won!');
+console.log(rps1('scissors','paper'), 'Player 1 won!');
+console.log(rps1('paper','paper'), 'Draw!');

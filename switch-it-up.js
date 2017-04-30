dbf0ed40546954181573622c1bc43c0f78513ad6
Sofia Nguy <sofianguy@gmail.com>
2017-04-30 11:08:54 -0700

    console.log()

--- switch-it-up.js
+++ switch-it-up.js
@@ -41,6 +41,6 @@ function switchItUp1(number) {
       return 'idk';
   }
 }
-console.log(switchItUp(1),"One");
-console.log(switchItUp(3),"Three");
-console.log(switchItUp(5),"Five");
+console.log(switchItUp1(1),"One");
+console.log(switchItUp1(3),"Three");
+console.log(switchItUp1(5),"Five");

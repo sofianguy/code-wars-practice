11120c70a41e5a5c0fd267f041c48b9c30cbd3b7
Sofia Nguy <sofianguy@gmail.com>
2017-04-29 21:04:18 -0700

    solve with switch/case

--- switch-it-up.js
+++ switch-it-up.js
@@ -14,3 +14,33 @@ function switchItUp(number) {
 console.log(switchItUp(1),"One");
 console.log(switchItUp(3),"Three");
 console.log(switchItUp(5),"Five");
+
+function switchItUp1(number) {
+  switch(number) {
+    case 0:
+      return 'Zero';
+    case 1:
+      return 'One';
+    case 2:
+      return 'Two';
+    case 3:
+      return 'Three';
+    case 4:
+      return 'Four';
+    case 5:
+      return 'Five';
+    case 6:
+      return 'Six';
+    case 7:
+      return 'Seven';
+    case 8:
+      return 'Eight';
+    case 9:
+      return 'Nine';
+    default:
+      return 'idk';
+  }
+}
+console.log(switchItUp(1),"One");
+console.log(switchItUp(3),"Three");
+console.log(switchItUp(5),"Five");

60a02a8a4efd8cf08984c1f93f803359b7bad495
Sofia Nguy <sofianguy@gmail.com>
2017-05-01 16:29:51 -0700

    print array elements with comma delimiters

---
+++ print-array-elements-with-comma-delimiters.js
@@ -0,0 +1,12 @@
+// Input: Array of elements
+//
+// ["h","o","l","a"]
+//
+// Output: String with comma delimited elements of the array in th same order.
+//
+// "h,o,l,a"
+
+function printArray(array) {
+  return array.join(',');
+}
+console.log(printArray(["h","o","l","a"]), 'h,o,l,a');

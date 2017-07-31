9fa21df39656c399091c299cd85386178e092800
Sofia Nguy <sofianguy@gmail.com>
2017-07-31 10:12:56 -0700

    deleted line

--- how-good-are-you-really.js
+++ how-good-are-you-really.js
@@ -20,7 +20,6 @@ function betterThanAverage(classPoints, yourPoints) {
   } else if (yourPoints <= classAvg) {
     return false;
   }
-
 }
 console.log(betterThanAverage([2, 3], 5), true);
 console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75), true);
